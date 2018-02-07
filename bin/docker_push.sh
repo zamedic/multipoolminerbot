0c567202775c844babc5775a2b9f51368bff6922
Marc Arndt <marc@marcarndt.com>
2018-02-07 18:31:38 +0200

    adding execute priverlages

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -4,12 +4,6 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Push only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "master" ]; then
 
-    # This is needed to login on AWS and push the image on ECR
-    # Change it accordingly to your docker repo
-    pip install --user awscli
-    export PATH=$PATH:$HOME/.local/bin
-    eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
-
     # Build and push
     docker build -t $IMAGE_NAME:1.$TRAVIS_JOB_NUMBER .
     echo "Pushing $IMAGE_NAME:2.$TRAVIS_JOB_NUMBER"
