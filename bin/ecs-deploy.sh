1dc48822019f605ad5234d70794337ab5882eed7
Marc Arndt <marc@marcarndt.com>
2018-02-07 19:47:46 +0200

    updating ecs deploy

--- bin/ecs-deploy.sh
+++ bin/ecs-deploy.sh
@@ -4,6 +4,9 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Deploy only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Deploying $TRAVIS_BRANCH on $TASK_DEFINITION"
+    pip install --user awscli
+    export PATH=$PATH:$HOME/.local/bin
+    eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
     ./bin/ecs-deploy -c $TASK_DEFINITION -n $SERVICE -i $IMAGE_NAME:1.$TRAVIS_JOB_NUMBER
   else
     echo "Skipping deploy because it's not an allowed branch"
